420d6b4a4e0ee8336b36305f60f9a5940912d751
Nolan Terpstra <nbakert@gmail.com>
2020-08-16 22:17:05 -0500

    Added 5 day forecast and currently only shows temp.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var cities = ["Houston"];
+var cities = [];
 const keyAPI = "8070a1de01ef42e1b1b72c9614f183cf"
+let lat;
+let lon;
 
 let currentCity = $("#currentCity");
 let temp = $("#temp");
@@ -10,7 +12,7 @@ function displayWeatherInfo(event) {
     event.preventDefault();
     var city = $(this).data("name");
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyAPI}`
-
+    // Current Weather
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -21,6 +23,26 @@ function displayWeatherInfo(event) {
         temp.text(`Temperature: ${tempF.toFixed(2)} F`);
         humidity.text(`Humidity: ${response.main.humidity}`);
         windSpeed.text(`Wind Speed: ${response.wind.speed} MPH`);
+        lat = response.coord.lat;
+        lon = response.coord.lon;
+    });
+
+    var query2URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${keyAPI}`
+    // 5 Day Forecast
+    $.ajax({
+        url: query2URL,
+        method: "GET"
+    }).then(function (response2) {
+        for (let i = 0; i < 5; i++) {
+            let tempK = (response2.list[i].main.temp);
+            let tempF = (tempK - 273.15) * 1.80 + 32;
+            let forecastDay = $("<div>");
+            forecastDay.text(`Day ${i+1}`)
+            let forecastTemp = $("<p>");
+            forecastTemp.text(`Temperature ${tempF.toFixed(2)} F`);
+            forecastDay.append(forecastTemp);
+            $("#forecast").append(forecastDay);
+        }
     });
 }
 
@@ -34,7 +56,7 @@ $("#searchButton").on("click", function (event) {
 function renderCityList() {
     $("#searchedCities").empty();
     for (let i = 0; i < cities.length; i++) {
-        var a = $("<button>");
+        var a = $("<li>");
         a.addClass("addedCity");
         a.attr("data-name", cities[i]);
         a.text(cities[i]);
